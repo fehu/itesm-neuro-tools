3ea09bce261c6c0270b6e71e75f6d54b8cfeadfe
fehu <lord.fehu@gmail.com>
2016-02-15 23:20:28 -0600

    using WildfireClass;

--- image-characteristics/exec/DescriptiveStatsAll.hs
+++ image-characteristics/exec/DescriptiveStatsAll.hs
@@ -15,15 +15,28 @@ import ImgCharacteristics.GTK.FromFriday
 
 import Vision.Image (RGB, HSV, Grey)
 
-fixedRegions = FixedColRowRegions{ rRow = 5
-                                 , rCol = 5
-                                 , minRegionSize = (200, 200)
+data WildfireClass = Fire
+                   | Smoke
+                   | FireAndSmoke
+                   | None
+                   | Ignore
+                   deriving (Enum, Bounded)
+
+instance Show WildfireClass where show Fire = "Fire"
+                                  show Smoke = "Smoke"
+                                  show FireAndSmoke = "Fire&Smoke"
+                                  show None = "Neither"
+                                  show Ignore = "Ignore"
+
+fixedRegions = FixedColRowRegions{ rRow = 50
+                                 , rCol = 50
+                                 , minRegionSize = (100, 100)
                                  }           -- (height, width)
 
 instance RegionsExtractor RGB where foreachRegion   = fixedColRowRegions fixedRegions
                                     foreachRegionIO = fixedColRowRegions fixedRegions
 
-main = do ci <- classProvider :: IO (ClassesInterview RGB Bool)
+main = do ci <- classProvider :: IO (ClassesInterview RGB WildfireClass)
 
           let eRGB  = extractorRGB  descriptiveStats
               eHSV  = extractorHSV  descriptiveStats
