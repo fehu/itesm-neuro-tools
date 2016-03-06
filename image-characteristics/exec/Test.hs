79a99bed3f62e0338e40695e760495ededaa80c6
fehu <lord.fehu@gmail.com>
2016-03-06 12:29:03 -0600

    'Execution' wraps exec. pool, inputs and outputs;

--- image-characteristics/exec/Test.hs
+++ image-characteristics/exec/Test.hs
@@ -11,11 +11,12 @@ import Data.Int
 import Control.Monad
 
 main = runJava $ inParallel 2 (void . J.forkJava) init f ([10..20] :: [Int32])
-    where init :: Java (Java a -> Java a)
-          init = fromIO . fmap (fromIO .) $ withWekaHomeEnv' extraClasspath
-          f :: (Java () -> Java ()) -> Int32 -> Java ()
-          f c v = c $ do Just s <- I.toHexString v
-                         println =<< toString s
+    where init :: Java (Java a -> Java a, [String])
+          init = fromIO $ do g <- (fromIO .) <$> withWekaHomeEnv' extraClasspath
+                             return (g, [])
+
+          f (f,_) v = f $ do Just s <- I.toHexString v
+                             println =<< toString s
 
 
 extraClasspath = [ "java/classes" ]
--- image-characteristics/src/ImgCharacteristics/ParallelExec.hs
+++ image-characteristics/src/ImgCharacteristics/ParallelExec.hs
@@ -29,6 +29,7 @@ import ImgCharacteristics
 
 import Control.Concurrent
 import Control.Concurrent.MVar
+import Control.Concurrent.STM
 import Control.Monad
 import Control.Arrow
 import Data.List (uncons)
@@ -37,13 +38,20 @@ import Data.IORef
 
 -----------------------------------------------------------------------------
 
+class (Monad a) =>  IOAlias a where toIO   :: a  x -> IO x
+                                    fromIO :: IO x -> a  x
+
+instance IOAlias IO where toIO = id
+                          fromIO = id
+
+-----------------------------------------------------------------------------
+
 type ForeachRegionM m img = forall a . img -> (img -> (Int, Int) -> m a) -> m [a]
 
 type ForeachRegionM' m img =
     forall a . img -> ((Int, Int), (Int,Int), (img -> (Int, Int) -> m a) -> m [a])
 
-class (Monad a) =>  IOAlias a where toIO   :: a  x -> IO x
-                                    fromIO :: IO x -> a  x
+-----------------------------------------------------------------------------
 
 -- | Process each region in parallell.
 class RegionsExtractorPar m img where foreachRegionPar  :: Int -- ^ Number of threads.
@@ -69,72 +77,102 @@ inParallel :: (Eq a, IOAlias m) =>
 inParallel poolSize forkExecutor init f args = do
     pool <- replicateM poolSize (fromIO $ newExecutor f init)
     forM_ pool (forkExecutor . runExecutor)
-    parrun pool args []
+    parrun pool args
 
 
 inParallel' :: (Eq a) =>
                Int           -- ^ Executor pool size.
-            -> (a -> m b)   -- ^ Function to be applied.
+            -> (a -> IO b)   -- ^ Function to be applied.
             -> [a]           -- ^ Data to apply.
-            -> m [b]
-inParallel' poolSize f = undefined -- inParallel poolSize (return ()) (const f)
+            -> IO [b]
+inParallel' poolSize f = inParallel poolSize (void . forkIO) (return ()) (const f)
 
 
 usedThreadDelay = 10 -- millis
 
-parrun :: (Eq a, IOAlias m) => [Executor m c a b] -> [a] -> [b] -> m [b]
-parrun pool args acc =
+parrun :: (Eq a, IOAlias m) => [Executor m c a b] -> [a] -> m [b]
+parrun pool args = do e <- newExecution pool args
+                      parrun' e []
+                      fromIO $ takeMVar $ execOutputs e
 
-    let traversePool []      as         = return ([], as)
+
+parrun' :: (Eq a, IOAlias m) => Execution m c a b -> [b] -> m ()
+parrun' e acc = do
+    (args, terminate) <- fromIO . atomically $ do arg <- readTVar $ execInputs e
+                                                  fin <- readTVar $ execTerminate e
+                                                  return (arg, fin)
+
+    let pool = execPool e
+        traversePool []      as         = return ([], as)
         traversePool (ph:pt) as = do
             let exec = writeInput ph
+                finish = exec $ if terminate then ETerminate else EFinish
 
             out <- readOutput ph
             waits <- waitingInput ph
 
             case out of Just v -> do t <- case as
                                             of ah:at -> exec (EInput ah) >> return at
-                                               _     -> exec EFinish     >> return []
+                                               _     -> finish           >> return []
                                      first (v:) <$> traversePool pt t
                         _      -> do t <- case as
                                             of ah:at | waits -> exec (EInput ah) >> return at
-                                               []    | waits -> exec EFinish     >> return []
+                                               []    | waits -> finish           >> return []
                                                a             -> return a
                                      traversePool pt t
 
 
-    in do (res, args') <- fromIO $ traversePool pool args
-          fromIO $ threadDelay usedThreadDelay
-          let acc' = res ++ acc
-          let recC = parrun pool args' acc'
-          if null args then do fin <- fromIO $ liftM and $ mapM doesNothing pool
-                               if fin then return $ reverse acc'
-                               else recC
-                       else recC
+    (res, args') <- fromIO $ traversePool pool args
+    fromIO $ threadDelay usedThreadDelay
+    let acc' = res ++ acc
+    let recC = parrun' e acc'
+    fromIO . atomically $ writeTVar (execInputs e) args'
+    if null args then do fin <- fromIO $ liftM and $ mapM doesNothing pool
+                         if fin then fromIO . putMVar (execOutputs e) $ reverse acc'
+                                else recC
+                 else recC
 
+-----------------------------------------------------------------------------
+
+data Execution m c a b = Execution {
+        execPool    :: [Executor m c a b]
+      , execInputs  :: TVar [a]
+      , execOutputs :: MVar [b]
+      , execTerminate :: TVar Bool
+    }
+
+newExecution pool args = fromIO $ do ein  <- newTVarIO args
+                                     eout <- newEmptyMVar
+                                     term <- newTVarIO False
+                                     return $ Execution pool ein eout term
 
 -----------------------------------------------------------------------------
 
 data ExecutorInput a = EInput a
+                     | ETerminate
                      | EFinish
                      deriving (Eq, Ord, Show)
 
 data Executor m c a b = Executor {
-        execTask    :: c -> a -> m b
-      , execInput   :: MVar (ExecutorInput a)
-      , execOutput  :: MVar b
-      , execContext     :: IORef (Maybe c)
-      , execContextInit :: m c
+        execTask   :: c -> a -> m b
+      , execInput  :: MVar (ExecutorInput a)
+      , execOutput :: MVar b
+      , execFinished   :: TVar Bool
+      , execTerminated :: TVar Bool
+      , execContext      :: IORef (Maybe c)
+      , execContextInit  :: m c
     }
 
 newExecutor f init = do ma <- newEmptyMVar
                         mb <- newEmptyMVar
+                        w  <- newTVarIO False
+                        t  <- newTVarIO False
                         c  <- newIORef Nothing
-                        return $ Executor f ma mb c init
+                        return $ Executor f ma mb w t c init
 
 writeInput = putMVar . execInput
 readOutput = tryTakeMVar . execOutput
-doesNothing e = (Just EFinish ==) <$> tryReadMVar (execInput e)
+doesNothing = readTVarIO . execFinished
 waitingInput = isEmptyMVar . execInput
 
 
@@ -148,10 +186,14 @@ runExecutor e = do -- Initialize if needed
                    -- Wait for the argument
                    arg <- fromIO $ takeMVar $ execInput e
 
+                   let setFin ps = forM_ ps $ \(sel, b) -> fromIO (atomically (writeTVar (sel e) b))
+
                    -- Apply to @f@ or finish.
-                   case arg of EInput a -> execTask e ctx a >>= (fromIO . putMVar (execOutput e))
-                                                            >> runExecutor e
-                               EFinish  -> fromIO . void $ putMVar (execInput e) EFinish
+                   case arg of EInput a -> setFin [(execFinished, False)]
+                                        >> execTask e ctx a >>= (fromIO . putMVar (execOutput e))
+                                        >> runExecutor e
+                               EFinish    -> setFin [(execFinished, True)] >> runExecutor e
+                               ETerminate -> setFin [(execFinished, True), (execTerminated, True)]
 
 
 -----------------------------------------------------------------------------
